9a4073b47bf8111fadef5deeb0b699d914e84a35
MD. ABU NOMAN <nomannabil30@gmail.com>
2021-12-15 21:16:27 +0600

    bKash
    Java

---
+++ bKash/src/bkash/CustomerFeedBackController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bkash;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author ASUS
+ */
+public class CustomerFeedBackController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
